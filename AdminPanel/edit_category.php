dcc4694a87edd18387527dcf30f4288460d76e34
laiba shahid <shahidlaiba26@gmail.com>
2026-06-19 20:42:10 +0500

    Added update Only Once Feature

--- AdminPanel/edit_category.php
+++ AdminPanel/edit_category.php
@@ -3,15 +3,18 @@ ob_start();
 ob_flush();
 include("../connection.php");
 include("header.php");
+$JoIDURLSeArahiHai = $_GET['id'];
 
+$query = "SELECT * FROM category where cate_id = '$JoIDURLSeArahiHai'";
+$res = mysqli_query($conn,$query);
+$data = mysqli_fetch_assoc($res);
 
-if(isset($_POST['btnAdd'])){
-    $name = $_POST['txtcatename']; // Minhaj
-    $query = "INSERT INTO category (`cate_name`,`is_updated`) VALUES ('$name','No')";
-    mysqli_query($conn,$query);
+if(isset($_POST['btnEdit'])){
+   $name =  $_POST['txtcatename']; // Cotton Boxes
+    $updateQuery = "Update category set cate_name = '$name',is_updated = 'Yes' where cate_id = '$JoIDURLSeArahiHai'";
+    mysqli_query($conn,$updateQuery);
     header("Location: show_category.php");
 }
-
 
 ?>
 
@@ -25,19 +28,19 @@ if(isset($_POST['btnAdd'])){
                     <div class="col-lg-12">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Add Category</h1>
+                                <h1 class="h4 text-gray-900 mb-4">Edit Category</h1>
                             </div>
                             <form class="user" method="post">
                                 <div class="form-group row">
                                     <div class="col-sm-12 mb-3 mb-sm-0">
                                         <input type="text" name="txtcatename" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="Category Name">
+                                            placeholder="Category Name" value="<?php echo $data['cate_name'] ?> ">
                                     </div>
                                   
                                 </div>
                                 
                                 
-                               <input type="submit" name="btnAdd" value="Add Category"  class="btn btn-primary btn-user btn-block">
+                               <input type="submit" name="btnEdit" value="Edit Category"  class="btn btn-primary btn-user btn-block">
                                 <hr>
                                
                                
--- AdminPanel/show_category.php
+++ AdminPanel/show_category.php
@@ -1,75 +1,109 @@
-<?PHP 
+<?PHP
 
 include("../connection.php");
 include("header.php");
 
 $query = "SELECT * FROM category";
-$result = mysqli_query($conn,$query);
-$data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+$result = mysqli_query($conn, $query);
+$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
 ?>
 
 
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 
 
-  <div class="container-fluid">
+<div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-                        For more information about DataTables, please visit the <a target="_blank"
-                            href="https://datatables.net">official DataTables documentation</a>.</p>
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+        For more information about DataTables, please visit the <a target="_blank"
+            href="https://datatables.net">official DataTables documentation</a>.</p>
 
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>Category ID</th>
-                                            <th>Category Name</th>
-                                            
-                                        </tr>
-                                    </thead>
-                                   
-                                    <tbody>
-                                        <?php 
-                                        foreach($data as $record) {
-                                        ?>
-                                            <tr>
-                                                <td>Hello</td>
-                                                <td>World</td>
-                                            </tr>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Category ID</th>
+                            <th>Category Name</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
 
+                    <tbody>
+                        <?php
+                        foreach ($data as $record) {
+                        ?>
+                            <tr>
+                                <td><?php
+                                    echo $record['cate_id']
+                                    ?></td>
+                                <td><?php
+                                    echo $record['cate_name']
+                                    ?></td>
+                                <td>
+                                    <?php 
+                                    if($record['is_updated'] == "Yes") {
+                                    ?>
 
-                                            <?php 
+                                    <a href="">
+                                        <button class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </a>
+                                    <?php  }
+                                        else{
+                                    ?>
+                                      <a href="">
+                                        <button class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </a>
+                                    <a href="edit_category.php?id=<?php
+                                                                    echo $record['cate_id']
+                                                                    ?>">
+                                        <button class="btn btn-warning">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+                                    </a>
+
+                                    <?php
                                         }
-                                            ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                                    ?>
+                                </td>
+                            </tr>
 
 
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 
-<?php 
+
+
+<?php
 include("footer.php")
 ?>
 
- <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<!-- Page level plugins -->
+<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
+<!-- Page level custom scripts -->
+<script src="js/demo/datatables-demo.js"></script>
